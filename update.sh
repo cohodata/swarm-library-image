da4a4422f43d5fe787b54f861d1dac2e4ce0880f
Daniel Ferstay <dan@cohodata.com>
2017-01-11 12:44:51 -0800

    Make build environment cleanup more robust
    Signed-off-by: Daniel Ferstay <dan@cohodata.com>

--- update.sh
+++ update.sh
@@ -7,6 +7,31 @@ if [ $# -eq 0 ] ; then
 	exit
 fi
 
+cleanup() {
+    EXITCODE=$?
+    echo "Cleaning up.."
+    # cleanup build directory
+    if [ ! -z "$TEMP" ]; then
+	rm -rf "$TEMP"
+    fi
+
+    # remove build containers
+    if [ ! -z "$BUILDER_ID" ]; then
+	docker rm -f "$BUILDER_ID" || :
+    fi
+    if [ ! -z "$WINBUILDER_ID" ]; then
+	docker rm -f "$WINBUILDER_ID" || :
+    fi
+
+    # remove build images
+    docker rmi swarm-builder:windows || :
+    docker rmi swarm-builder || :
+
+    echo "Done."
+    return $EXITCODE
+}
+trap 'cleanup' 0
+
 SWARM_REPO=${SWARM_REPO-https://github.com/docker/swarm.git}
 
 VERSION=$1
@@ -27,28 +52,18 @@ git clone -b $VERSION $SWARM_REPO $TEMP
 docker build -t swarm-builder $TEMP
 
 # Create a dummy swarmbuild container so we can run a cp against it.
-ID=$(docker create swarm-builder)
+BUILDER_ID=$(docker create swarm-builder)
 
 # Update the local binary.
-docker cp $ID:/go/bin/swarm .
-
-# Cleanup.
-docker rm -f $ID
-docker rmi swarm-builder
+docker cp $BUILDER_ID:/go/bin/swarm .
 
 echo "Building swarm.exe $VERSION..."
 
 docker build -t swarm-builder:windows --build-arg GOOS=windows $TEMP
 
 # Create a dummy swarmbuild container so we can run a cp against it.
-ID=$(docker create swarm-builder:windows)
+WINBUILDER_ID=$(docker create swarm-builder:windows)
 
 # Update the local binary.
-docker cp $ID:/go/bin/windows_amd64/swarm.exe .
+docker cp $WINBUILDER_ID:/go/bin/windows_amd64/swarm.exe .
 mv swarm.exe swarm-unsupported.exe
-
-# Cleanup.
-docker rm -f $ID
-docker rmi swarm-builder:windows
-
-echo "Done."
